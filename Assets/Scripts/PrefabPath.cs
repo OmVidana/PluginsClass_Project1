52f51e055e3bcf46da8b068cd33a288bd51fb237
Migue0244655 <112912731+Migue0244655@users.noreply.github.com>
2023-02-26 22:52:35 -0600

    Game Background Done
    Falta boton para salir

---
+++ Assets/Scripts/PrefabPath.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabPath : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > 1)
+        {
+            transform.position += new Vector3(0, 0, Random.Range(-3f, -6f)) * Time.deltaTime;
+        }
+        if (transform.position.z <= 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
